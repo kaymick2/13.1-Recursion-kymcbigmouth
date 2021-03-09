55d7b274970a852615d3c7129f704f54341cca60
Kyle McDowell <kymcd21@icstudents.org>
2021-03-09 16:31:39 +0000

    triangle

--- Main.java
+++ Main.java
@@ -2,16 +2,30 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    Scanner numberInput=new Scanner(System.in);
-    System.out.println("Please enter a number: ");
-    double factNumber=numberInput.nextDouble();
+    Scanner numberInput = new Scanner(System.in);
+    System.out.println("Please enter a number for factorials: ");
+    double factNumber = numberInput.nextDouble();
+    System.out.print("you have entered " + factNumber + ". The factorial is: ");
     System.out.println(factorial(factNumber));
+    System.out.println("please enter a number for triangle number thingy: ");
+    double triNumber = numberInput.nextDouble();
+    System.out.println(triangle(triNumber));
+    System.out.print("you have entered " + triNumber + ". The sum is: ");
   }
-public static double factorial(double number){
-  if(number==0){
-    return 1;
-  }else{
-    return number* factorial(number-1);
+
+  public static double factorial(double number) {
+    if (number == 0) {
+      return 1;
+    } else {
+      return number * factorial(number - 1);
+    }
   }
-}
+
+  public static double triangle(double number) {
+    if (number == 0) {
+      return 1;
+    } else {
+      return number + factorial(number - 1);
+    }
+  }
 }
