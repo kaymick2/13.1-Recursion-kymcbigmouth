5664b9e7125d9cc4c4e60f3f51182fcf57ecde2c
Kyle McDowell <kymcd21@icstudents.org>
2021-03-09 16:17:29 +0000

    VERSÃO 1

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    
-    
-
-    
+    Scanner numberInput=new Scanner(System.in);
+    System.out.println("Please enter a number: ");
+    double factNumber=numberInput.nextDouble();
+    System.out.println(factorial(factNumber));
   }
+public static double factorial(double number){
+  if(number==0){
+    return 1;
+  }else{
+    return number* factorial(number-1);
+  }
+}
 }
